388fa4082b8a02c6fc203b82857044cad2bb4c05
Ehsan Jalali <JMSEhsan@gmail.com>
2021-10-26 23:17:44 -0600

    Initialize Web API practice Repo

---
+++ script.js
@@ -0,0 +1,32 @@
+/* Practice using Giphy API     Oct.24, 2021
+Ref.: Introduction to Web APIs” by Andrew Probert on LinekedIn
+Ex_Files_Introduction_Web_APIs */
+
+
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://hplussport.com/api/products?order=name');
+
+request.onload = function() {
+	var response = request.response;
+	var parsedData = JSON.parse(response);
+	console.log(parsedData);
+	
+	for(item in parsedData) {
+		
+		//Display all the product names
+		var name = parsedData[0].name;
+		var products = document.createElement('li');
+		products.innerHTML = name;
+		document.body.appendChild(products);
+
+		//Display all the product images
+		var imageUrl = parsedData[item].image;
+		var images = document.createElement('img');
+		images.setAttribute('src', imageUrl);
+		document.body.appendChild(images);
+
+	}
+};
+
+request.send();
